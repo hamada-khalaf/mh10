f9ed351020b11592f7902b5b5d4ad6631bba29b4
hamada-khalaf <103873622+hamada-khalaf@users.noreply.github.com>
2022-05-11 03:32:38 +0200

    Add files via upload

---
+++ Common/footer.php
@@ -0,0 +1,46 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>SVA College</h3>
+            <p>This Project has been made by Hamada Khalaf Abd Elhalim with the help of 
+              my Allah Almighty.
+              The prupose of this project is Learning Computer Languages.
+              I hope you realy enjoyed to visit this project. Thanks!
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About Sva</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Sc. Civil Engineering</a></li>
+              <li><a href="">B.Sc. Electrical Engineering</a></li>
+              <li><a href="">B.Sc. Mechanical Engineering</a></li>
+              <li><a href="">B. Architecture</a></li>
+              <li><a href="">BS Computer Science</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: sva College of Business Studies,
+              Obour City - Ahmed Orabi Association - Gate 7 - Road 8 North</p>
+              <p>Telephones: (+201033855774)</p>
+              <p>E-mail: hm20160249@sva.edu.eg</p>
+        </div>
+      </div>
+    </div>
+    </div>
+         
+  <p class="footer-bottom-text">CopyRights © reserved by hamada khalaf Programming and Code hamada khalaf.2022</p>
+</footer>
